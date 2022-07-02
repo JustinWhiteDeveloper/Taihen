17e57d348eb88312c2aaee2b99bb32fa0803022f
Justin White <justin@jwhite.co.nz>
2022-07-02 18:54:54 +1200

    [TAIH-2] Cleanup Feature Manager constants

--- Taihen/Taihen/Generic/FeatureManager.swift
+++ Taihen/Taihen/Generic/FeatureManager.swift
@@ -1,6 +1,29 @@
 import Foundation
 import SwiftUI
 
+private enum Strings {
+    static let audioEnabled = "AudioEnabled"
+    static let audioPlayAudio = "AutoPlayAudio"
+    static let enableTextHighlights = "EnableTextHighlights"
+    static let enablePositionScrolling = "PostionScrolling"
+    
+    static let lookupPreviewEnabled = "LookupPreviewEnabled"
+    static let clipboardEnabled = "ClipboardEnabled"
+    
+    static let readerTextSize = "ReaderSize"
+    static let dictionaryTextSize = "MiniReaderSize"
+    
+    static let readerBackgroundColorR = "BGColorR"
+    static let readerBackgroundColorG = "BGColorG"
+    static let readerBackgroundColorB = "BGColorB"
+    
+    static let japaneseParserMode = "JPParserMode"
+}
+
+private enum Sizings {
+    static let defaultTextSize = 30.0
+}
+
 class FeatureManager {
     
     static let instance: FeatureManager = FeatureManager()
@@ -9,109 +32,109 @@ class FeatureManager {
     
     var audioEnabled: Bool {
         get {
-            userDefaults.bool(forKey: "AudioEnabled")
+            userDefaults.bool(forKey: Strings.audioEnabled)
         }
         
         set {
-            userDefaults.set(newValue, forKey: "AudioEnabled")
+            userDefaults.set(newValue, forKey: Strings.audioEnabled)
         }
     }
     
     var autoplayAudio: Bool {
         get {
-            userDefaults.bool(forKey: "AutoPlayAudio")
+            userDefaults.bool(forKey: Strings.audioPlayAudio)
         }
         
         set {
-            userDefaults.set(newValue, forKey: "AutoPlayAudio")
+            userDefaults.set(newValue, forKey: Strings.audioPlayAudio)
         }
     }
     
     var enableTextHighlights: Bool {
         get {
-            userDefaults.bool(forKey: "EnableTextHighlights")
+            userDefaults.bool(forKey: Strings.enableTextHighlights)
         }
         
         set {
-            userDefaults.set(newValue, forKey: "EnableTextHighlights")
+            userDefaults.set(newValue, forKey: Strings.enableTextHighlights)
         }
     }
     
     var positionScrolling: Bool {
         get {
-            userDefaults.bool(forKey: "PostionScrolling")
+            userDefaults.bool(forKey: Strings.enablePositionScrolling)
         }
         
         set {
-            userDefaults.set(newValue, forKey: "PostionScrolling")
+            userDefaults.set(newValue, forKey: Strings.enablePositionScrolling)
         }
     }
     
     var lookupPreviewEnabled: Bool {
         get {
-            userDefaults.bool(forKey: "LookupPreviewEnabled")
+            userDefaults.bool(forKey: Strings.lookupPreviewEnabled)
         }
         
         set {
-            userDefaults.set(newValue, forKey: "LookupPreviewEnabled")
+            userDefaults.set(newValue, forKey: Strings.lookupPreviewEnabled)
         }
     }
     
     var clipboardReadingEnabled: Bool {
         get {
-            (userDefaults.object(forKey: "ClipboardEnabled") as? Bool) ?? true
+            (userDefaults.object(forKey: Strings.clipboardEnabled) as? Bool) ?? true
         }
         
         set {
-            userDefaults.set(newValue, forKey: "ClipboardEnabled")
+            userDefaults.set(newValue, forKey: Strings.clipboardEnabled)
         }
     }
     
     var readerTextSize: Double {
         get {
-            let value = userDefaults.double(forKey: "ReaderSize")
+            let value = userDefaults.double(forKey: Strings.readerTextSize)
             
             if value == 0 {
-                return 30.0
+                return Sizings.defaultTextSize
             } else {
                 return value
             }
         }
         
         set {
-            userDefaults.set(newValue, forKey: "ReaderSize")
+            userDefaults.set(newValue, forKey: Strings.readerTextSize)
         }
     }
     
     var dictionaryTextSize: Double {
         get {
-            let value = userDefaults.double(forKey: "MiniReaderSize")
+            let value = userDefaults.double(forKey: Strings.dictionaryTextSize)
             
             if value == 0 {
-                return 30.0
+                return Sizings.defaultTextSize
             } else {
                 return value
             }
         }
         
         set {
-            userDefaults.set(newValue, forKey: "MiniReaderSize")
+            userDefaults.set(newValue, forKey: Strings.dictionaryTextSize)
         }
     }
     
     var readerBackgroundColor: Color {
         get {
-            let valueR = userDefaults.double(forKey: "BGColorR")
-            let valueG = userDefaults.double(forKey: "BGColorG")
-            let valueB = userDefaults.double(forKey: "BGColorB")
+            let valueR = userDefaults.double(forKey: Strings.readerBackgroundColorR)
+            let valueG = userDefaults.double(forKey: Strings.readerBackgroundColorG)
+            let valueB = userDefaults.double(forKey: Strings.readerBackgroundColorB)
 
             return Color(red: valueR, green: valueG, blue: valueB)
         }
         
         set {
-            userDefaults.set(newValue.cgColor?.components?[0] ?? 0, forKey: "BGColorR")
-            userDefaults.set(newValue.cgColor?.components?[1] ?? 0, forKey: "BGColorG")
-            userDefaults.set(newValue.cgColor?.components?[2] ?? 0, forKey: "BGColorB")
+            userDefaults.set(newValue.cgColor?.components?[0] ?? 0, forKey: Strings.readerBackgroundColorR)
+            userDefaults.set(newValue.cgColor?.components?[1] ?? 0, forKey: Strings.readerBackgroundColorG)
+            userDefaults.set(newValue.cgColor?.components?[2] ?? 0, forKey: Strings.readerBackgroundColorB)
         }
     }
     
@@ -123,13 +146,13 @@ class FeatureManager {
     
     var parserMode: TempJapaneseParserMode {
         get {
-            let value = userDefaults.integer(forKey: "JPParserMode")
+            let value = userDefaults.integer(forKey: Strings.japaneseParserMode)
             
             return TempJapaneseParserMode(rawValue: value) ?? .Rule
         }
         
         set {
-            userDefaults.set(newValue.rawValue, forKey: "JPParserMode")
+            userDefaults.set(newValue.rawValue, forKey: Strings.japaneseParserMode)
         }
     }
     
